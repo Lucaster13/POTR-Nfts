111f7d938bb591dc441a82df62b65d35cef8e538
Lucas Terr <terr@usc.edu>
2023-01-20 23:59:43 -0500

    add asaIds

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -1,9 +1,9 @@
 // Import the NFTStorage class and File constructor from the 'nft.storage' package
 import { NFTStorage } from "nft.storage";
+import { CoinType } from "potr-utils/types";
 import { NFT_STORAGE_API_KEY } from "./.secrets.js";
 import { IPFS_PATH_PREFIX } from "./constants.js";
 import { getCids } from "./data.js";
-import { CoinType } from "potr-utils/types";
 
 // create nft storage
 const getNftStorage = () => new NFTStorage({ token: NFT_STORAGE_API_KEY });
